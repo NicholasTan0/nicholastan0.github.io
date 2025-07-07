cf03cb1f316cb964ca41ed5ded71bedfc3181677
Nicholas Tan <143367030+nktanSBU@users.noreply.github.com>
2025-07-07 16:40:45 -0400

    Fix: Mobile responsive

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
                 <h2 className="text-lg font-mono">Hi, my name is</h2>
                 <h1 className="max-w-fit typing text-responsive font-bold">Nicholas Tan.</h1>
             </div>
-            <div className="appearFirst flex flex-2/5 flex-col justify-center mb-8">
+            <div className="appearFirst flex flex-2/5 flex-col justify-center my-8">
                 <h2 className="text-lg text-white">About Me</h2>
                 <p className="text-[#ddd] pt-2 pb-4">I am a fourth year student studying Computer Science at Stony Brook University. I am passionate about full-stack development, game development, artificial intelligence, and UI/UX design.</p>
                 <a href="#about" className="flex items-center border-b-2 w-max text-white">Learn More
@@ -19,7 +19,7 @@ const Home = () => {
                     <svg width="32px" height="32px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="white"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"></path> </g></svg>
                 </a>
             </div>
-            <a href="#about" className="svg-down absolute bottom-[-3.5vh] left-[48.5%] z-10 bg-[rgba(255,255,255,0.4)] p-2 rounded-full">
+            <a href="#about" className="svg-down absolute bottom-[-3.5vh] left-[calc(50%-19px)] z-10 bg-[rgba(255,255,255,0.4)] p-2 rounded-full">
                 <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="32px" height="32px" viewBox="0 0 268.832 268.832" xmlSpace="preserve"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M223.255,167.493c-4.881-4.882-12.797-4.882-17.678,0l-58.661,58.661V12.5c0-6.903-5.598-12.5-12.5-12.5 c-6.904,0-12.5,5.597-12.5,12.5v213.654l-58.661-58.659c-4.883-4.881-12.797-4.881-17.678,0c-4.883,4.882-4.883,12.796,0,17.678 l80,79.998c2.439,2.44,5.64,3.661,8.839,3.661s6.397-1.221,8.839-3.661l80-80C228.137,180.289,228.137,172.375,223.255,167.493z"></path> </g> </g></svg>
             </a>
         </section>
--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,15 +1,16 @@
 import p1 from '../assets/whodat.png';
+// import p2 from '../assets/.png';
 
 const Portfolio = () => {
     return (
-        <section id="portfolio" className="flex flex-col gap-16 justify-center items-center w-screen bg-[#ddd] p-20">
-            <h1 className="text-gray-800 text-3xl m-2">My Projects</h1>
-                <div className="flex flex-wrap bg-gray-200 rounded-3xl shadow-xl/20 font-mont text-gray-800 leading-10 p-10 max-w-7xl">
+        <section id="portfolio" className="flex flex-col gap-14 justify-center items-center w-screen bg-[#ddd] p-20">
+            <h1 className="text-gray-800 text-3xl text-center">My Projects</h1>
+                <div className="flex flex-wrap bg-gray-200 rounded-3xl shadow-xl/20 font-mont text-gray-800 leading-10 p-10 max-w-[90vw]">
                     <div className='flex flex-col justify-between w-full min-[1280px]:w-1/2'>
                         <div>
                             <h1 className="text-3xl font-medium my-4">NBA Player Guessing Game</h1>
                             <p>An engaging, fast-paced NBA guessing game that challenges players to identify NBA players based on headshots and data parsed from ESPN. A serverless backend efficiently delivers player information, supporting customizable hints to suit individual preferences and difficulty levels.</p>
-                            <div className='flex gap-1.5 font-bold text-sm my-4'>
+                            <div className='flex flex-wrap gap-1.5 font-bold text-sm my-4'>
                                 <span>#React</span>
                                 <span>#JavaScript</span>
                                 <span>#NodeJS</span>
@@ -18,7 +19,7 @@ const Portfolio = () => {
                                 <span>#CSS</span>
                             </div>
                         </div>
-                        <div className='flex gap-4 my-4'>
+                        <div className='flex gap-4 my-4 flex-wrap'>
                             <a href="https://whodatnba.vercel.app" target="_blank" rel="noopener noreferrer" className='flex items-center gap-2 w-min py-1 px-4 border-1 rounded-full text-blue-700 transition-all duration-300 hover:bg-blue-700 hover:text-gray-200 active:scale-95'>
                                 <svg fill='currentColor' width="1rem" height="1rem" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M32.032 16c0-8.501-6.677-15.472-15.072-15.969-0.173-0.019-0.346-0.032-0.523-0.032-0.052 0-0.104 0.005-0.156 0.007-0.093-0.002-0.186-0.007-0.281-0.007-8.84 0-16.032 7.178-16.032 16.001s7.192 16.001 16.032 16.001c0.094 0 0.188-0.006 0.281-0.008 0.052 0.002 0.104 0.008 0.156 0.008 0.176 0 0.349-0.012 0.523-0.032 8.395-0.497 15.072-7.468 15.072-15.969zM29.049 21.151c-0.551-0.16-1.935-0.507-4.377-0.794 0.202-1.381 0.313-2.84 0.313-4.357 0-1.196-0.069-2.354-0.197-3.469 3.094-0.37 4.45-0.835 4.54-0.867l-0.372-1.050c0.695 1.659 1.080 3.478 1.080 5.386 0 1.818-0.352 3.555-0.987 5.151zM8.921 16c0-1.119 0.074-2.212 0.21-3.263 1.621 0.127 3.561 0.222 5.839 0.243v6.939c-2.219 0.021-4.114 0.111-5.709 0.234-0.22-1.319-0.34-2.715-0.34-4.154zM16.967 2.132c2.452 0.711 4.552 4.115 5.492 8.628-1.512 0.12-3.332 0.209-5.492 0.229v-8.857zM14.971 2.156v8.832c-2.136-0.021-3.965-0.109-5.502-0.226 0.96-4.457 3.076-7.836 5.502-8.606zM14.971 21.913l0 7.929c-2.263-0.718-4.256-3.705-5.293-7.719 1.492-0.11 3.253-0.189 5.292-0.21zM16.967 29.868l-0-7.955c2.061 0.020 3.814 0.102 5.288 0.217-1.019 4.067-3 7.076-5.288 7.738zM16.967 19.92l0-6.939c2.291-0.021 4.218-0.118 5.818-0.25 0.131 1.053 0.203 2.147 0.203 3.268 0 1.442-0.116 2.84-0.329 4.16-1.575-0.128-3.462-0.219-5.692-0.24zM28.588 9.81c-0.302 0.094-1.564 0.453-4.094 0.751-0.564-2.998-1.584-5.561-2.91-7.412 3.048 1.325 5.535 3.697 7.005 6.661zM11.213 2.831c-1.632 1.873-2.963 4.568-3.691 7.754-2.265-0.245-3.623-0.534-4.166-0.665 1.585-3.27 4.407-5.836 7.856-7.088zM2.614 11.787c0.385 0.104 1.841 0.467 4.549 0.766-0.155 1.107-0.24 2.26-0.24 3.447 0 1.509 0.136 2.96 0.383 4.334-2.325 0.251-3.755 0.552-4.396 0.706-0.607-1.566-0.944-3.264-0.944-5.041 0-1.467 0.228-2.883 0.649-4.213zM3.784 22.886c0.727-0.154 2.029-0.39 3.956-0.591 0.759 2.803 1.993 5.175 3.473 6.874-3.16-1.148-5.79-3.398-7.429-6.282v0zM21.583 28.849c1.195-1.665 2.14-3.907 2.728-6.525 1.982 0.227 3.226 0.494 3.853 0.652-1.5 2.596-3.808 4.669-6.581 5.873z"></path> </g></svg>
                                 <span>Demo</span>
@@ -29,35 +30,30 @@ const Portfolio = () => {
                             </a>
                         </div>
                     </div>
-                    <img src={p1} className='w-full min-[1280px]:w-1/2 h-auto'/>
+                    <img src={p1} alt='WhoDat' className='w-full min-[1280px]:w-1/2 h-auto'/>
                 </div>
 
-                <div className="flex flex-wrap bg-gray-200 rounded-3xl shadow-xl/20 font-mont text-gray-800 leading-10 p-10 max-w-7xl">
+                                <div className="flex flex-wrap bg-gray-200 rounded-3xl shadow-xl/20 font-mont text-gray-800 leading-10 p-10 max-w-[90vw]">
                     <div className='flex flex-col justify-between w-full min-[1280px]:w-1/2'>
                         <div>
-                            <h1 className="text-3xl font-medium my-4">Flashcard App</h1>
-                            <p></p>
-                            <div className='flex gap-1.5 font-bold text-sm my-4'>
-                                <span>#React</span>
-                                <span>#JavaScript</span>
-                                <span>#NodeJS</span>
-                                <span>#ExpressJS</span>
-                                <span>#HTML</span>
-                                <span>#CSS</span>
+                            <h1 className="text-3xl font-medium my-4">Title</h1>
+                            <p>Description</p>
+                            <div className='flex flex-wrap gap-1.5 font-bold text-sm my-4'>
+                                <span>Tech Stack</span>
                             </div>
                         </div>
-                        <div className='flex gap-4 my-4'>
-                            <a href="https://whodatnba.vercel.app" target="_blank" rel="noopener noreferrer" className='flex items-center gap-2 w-min py-1 px-4 border-1 rounded-full text-blue-700 transition-all duration-300 hover:bg-blue-700 hover:text-gray-200 active:scale-95'>
+                        <div className='flex gap-4 my-4 flex-wrap'>
+                            <a href="" target="_blank" rel="noopener noreferrer" className='flex items-center gap-2 w-min py-1 px-4 border-1 rounded-full text-blue-700 transition-all duration-300 hover:bg-blue-700 hover:text-gray-200 active:scale-95'>
                                 <svg fill='currentColor' width="1rem" height="1rem" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M32.032 16c0-8.501-6.677-15.472-15.072-15.969-0.173-0.019-0.346-0.032-0.523-0.032-0.052 0-0.104 0.005-0.156 0.007-0.093-0.002-0.186-0.007-0.281-0.007-8.84 0-16.032 7.178-16.032 16.001s7.192 16.001 16.032 16.001c0.094 0 0.188-0.006 0.281-0.008 0.052 0.002 0.104 0.008 0.156 0.008 0.176 0 0.349-0.012 0.523-0.032 8.395-0.497 15.072-7.468 15.072-15.969zM29.049 21.151c-0.551-0.16-1.935-0.507-4.377-0.794 0.202-1.381 0.313-2.84 0.313-4.357 0-1.196-0.069-2.354-0.197-3.469 3.094-0.37 4.45-0.835 4.54-0.867l-0.372-1.050c0.695 1.659 1.080 3.478 1.080 5.386 0 1.818-0.352 3.555-0.987 5.151zM8.921 16c0-1.119 0.074-2.212 0.21-3.263 1.621 0.127 3.561 0.222 5.839 0.243v6.939c-2.219 0.021-4.114 0.111-5.709 0.234-0.22-1.319-0.34-2.715-0.34-4.154zM16.967 2.132c2.452 0.711 4.552 4.115 5.492 8.628-1.512 0.12-3.332 0.209-5.492 0.229v-8.857zM14.971 2.156v8.832c-2.136-0.021-3.965-0.109-5.502-0.226 0.96-4.457 3.076-7.836 5.502-8.606zM14.971 21.913l0 7.929c-2.263-0.718-4.256-3.705-5.293-7.719 1.492-0.11 3.253-0.189 5.292-0.21zM16.967 29.868l-0-7.955c2.061 0.020 3.814 0.102 5.288 0.217-1.019 4.067-3 7.076-5.288 7.738zM16.967 19.92l0-6.939c2.291-0.021 4.218-0.118 5.818-0.25 0.131 1.053 0.203 2.147 0.203 3.268 0 1.442-0.116 2.84-0.329 4.16-1.575-0.128-3.462-0.219-5.692-0.24zM28.588 9.81c-0.302 0.094-1.564 0.453-4.094 0.751-0.564-2.998-1.584-5.561-2.91-7.412 3.048 1.325 5.535 3.697 7.005 6.661zM11.213 2.831c-1.632 1.873-2.963 4.568-3.691 7.754-2.265-0.245-3.623-0.534-4.166-0.665 1.585-3.27 4.407-5.836 7.856-7.088zM2.614 11.787c0.385 0.104 1.841 0.467 4.549 0.766-0.155 1.107-0.24 2.26-0.24 3.447 0 1.509 0.136 2.96 0.383 4.334-2.325 0.251-3.755 0.552-4.396 0.706-0.607-1.566-0.944-3.264-0.944-5.041 0-1.467 0.228-2.883 0.649-4.213zM3.784 22.886c0.727-0.154 2.029-0.39 3.956-0.591 0.759 2.803 1.993 5.175 3.473 6.874-3.16-1.148-5.79-3.398-7.429-6.282v0zM21.583 28.849c1.195-1.665 2.14-3.907 2.728-6.525 1.982 0.227 3.226 0.494 3.853 0.652-1.5 2.596-3.808 4.669-6.581 5.873z"></path> </g></svg>
                                 <span>Demo</span>
                             </a>
-                            <a href="https://github.com/NicholasTan0/WhoDat" target="_blank" rel="noopener noreferrer" className='flex items-center gap-2 w-min py-1 px-4 border-1 rounded-full transition-all duration-300 text-[var(--secondary)] hover:bg-[var(--secondary)] hover:text-gray-200 active:scale-95'>
+                            <a href="" target="_blank" rel="noopener noreferrer" className='flex items-center gap-2 w-min py-1 px-4 border-1 rounded-full transition-all duration-300 text-[var(--secondary)] hover:bg-[var(--secondary)] hover:text-gray-200 active:scale-95'>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" fill="currentColor" viewBox="0 0 16 16"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/></svg>
                                 <span>GitHub</span>
                             </a>
                         </div>
                     </div>
-                    {/* <img src={p1} className='w-full min-[1280px]:w-1/2 h-auto'/> */}
+                    <img src={null} alt='Image' className='w-full min-[1280px]:w-1/2 h-auto'/>
                 </div>
         </section>
     )
