63d3180041114d5646f5ba4ae81431a86e9064ff
Nicholas Tan <143367030+nktanSBU@users.noreply.github.com>
2025-07-07 02:34:28 -0400

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import About from './sections/About'
+import Navbar from './components/Navbar'
 import './style.css'
+import Home from './sections/Home'
+import Footer from './components/Footer'
+import Portfolio from './sections/Portfolio'
 
 function App() {
 
   return (
     <>
+      {/* <Navbar/> */}
+      <Home/>
+      <Portfolio/>
+      <About/>
+      <Footer/>
     </>
   )
 }
